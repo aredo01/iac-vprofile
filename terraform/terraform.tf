0b4bbff992c073bdfd540e69e084c430b005b221
aredo01 <181214873+aredo01@users.noreply.github.com>
2025-05-31 12:27:31 +0200

    changes on tf and var files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.79.0"
     }
 
     random = {
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofile-actions-14"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
